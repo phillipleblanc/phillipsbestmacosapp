26d8ebc825f4e96a6ed82b659c55140cc7c4d2ca
Phillip LeBlanc <phillip.leblanc@microsoft.com>
2017-11-07 16:21:13 +0900

    Adding project

---
+++ PhillipsBestMacApp/PhillipsBestMacApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhillipsBestMacApp
+//
+//  Created by Phillip LeBlanc on 11/7/17.
+//  Copyright © 2017 Phillip LeBlanc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PhillipsBestMacApp/PhillipsBestMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhillipsBestMacApp
+//
+//  Created by Phillip LeBlanc on 11/7/17.
+//  Copyright © 2017 Phillip LeBlanc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
