f4844f7af6ba442f8e1771f16b33900e2a6cc362
Phillip LeBlanc <phillip.leblanc@microsoft.com>
2019-12-02 14:34:06 +0900

    Adding Sparkle

---
+++ PhillipsBestMacApp/Pods/Target Support Files/Pods-PhillipsBestMacApp-PhillipsBestMacAppUITests/Pods-PhillipsBestMacApp-PhillipsBestMacAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhillipsBestMacApp_PhillipsBestMacAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PhillipsBestMacApp_PhillipsBestMacAppUITests
+@end
---
+++ PhillipsBestMacApp/Pods/Target Support Files/Pods-PhillipsBestMacAppTests/Pods-PhillipsBestMacAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhillipsBestMacAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_PhillipsBestMacAppTests
+@end
